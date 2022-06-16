acd9854bb5095ce537006286dd281373057c376a
shraddha gediya <rupesh.halpati@aspiresoftserv.com>
2022-06-16 18:53:29 +0530

    create database

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require('./models/post')
 
 const app = express();
+
+mongoose.connect("mongodb+srv://shraddha16:Shraddha26@cluster0.bz78p.mongodb.net/node-angular?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,30 +31,90 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
-    message: 'Post added successfully'
+    message: 'Post added successfully',
+    data : post
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
+  Post.find()
+  .then(documents =>{
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents
+    });
+
   });
+
 });
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const bodyParser = require("body-parser");
+
+// const Post = require('./models/post')
+
+// const app = express();
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PATCH, DELETE, OPTIONS"
+//   );
+//   next();
+// });
+
+// app.post("/api/posts", (req, res, next) => {
+//   const post = req.body;
+//   console.log(post);
+//   res.status(201).json({
+//     message: 'Post added successfully'
+//   });
+// });
+
+// app.get("/api/posts", (req, res, next) => {
+//   const posts = [
+//     {
+//       id: "fadf12421l",
+//       title: "First server-side post",
+//       content: "This is coming from the server"
+//     },
+//     {
+//       id: "ksajflaj132",
+//       title: "Second server-side post",
+//       content: "This is coming from the server!"
+//     }
+//   ];
+//   res.status(200).json({
+//     message: "Posts fetched successfully!",
+//     posts: posts
+//   });
+// });
+
+// module.exports = app;
